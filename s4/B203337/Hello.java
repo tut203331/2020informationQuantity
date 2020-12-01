4309f9f331c8384933167e1b88f178dced52dfc1
KOU256 <kou256.8bit+github@gmail.com>
2020-12-01 16:49:48 +0900

    Hello.javaで出力する文字を変更
    プログラムの実行時に出力される文字を任意の文字列に変更した。

--- s4/B203337/Hello.java
+++ s4/B203337/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello..(B203337)");
+        System.out.println("Hello, World! I'm B203337");
     }
 }
 
